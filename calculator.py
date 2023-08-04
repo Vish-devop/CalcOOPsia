ff9e00b88b763e1a6f4dfea2e5466f5f2d2c6ae1
vishal SHARMA <vishal.473079@gmail.com>
2023-08-04 11:35:58 +0530

    added the calculator class

---
+++ calculator.py
@@ -0,0 +1,20 @@
+class Calculator:
+    def __init__ (self):
+        pass
+    # def get_number(self):
+
+    def get_user_input(self):
+        num1=int(input("Enter the first Number: "))
+        num2=int(input("Enter the second Number: "))
+        return num1,num2
+        
+    def addition(self,num1,num2):
+        result = num1+num2
+        return result
+    
+
+    
+
+
+
+    
